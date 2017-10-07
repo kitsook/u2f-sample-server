0a5e90cd17df4ee3b6eee609747e5694ba5a0160
dabura667 <matsuringo667@gmail.com>
2017-10-07 23:16:25 +0900

    Fix dependencies and add start

--- index.js
+++ index.js
@@ -162,5 +162,5 @@ var server = https
     var host = server.address().address;
     var port = server.address().port;
 
-    console.log("U2F demo app started at http://%s:%s", host, port);
+    console.log("U2F demo app started at https://%s:%s/demo", host == "127.0.0.1" ? 'localhost' : host, port);
 });
